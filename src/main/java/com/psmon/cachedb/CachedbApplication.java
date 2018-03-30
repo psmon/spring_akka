3b758f1d0ef90ff06380ea73a100b4849a717559
psmon <psmon@live.co.kr>
2018-03-31 06:58:58 +0900

    fix

--- src/main/java/com/psmon/cachedb/CachedbApplication.java
+++ src/main/java/com/psmon/cachedb/CachedbApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.ApplicationContext;
 import com.psmon.cachedb.extension.SpringExtension;
 
 import akka.actor.ActorRef;
+
+import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
@@ -24,10 +26,23 @@ public class CachedbApplication {
         log.info("Starting up");
 
         SpringExtension ext = context.getBean(SpringExtension.class);
-                
-        ActorRef testActor = system.actorOf(ext.props("testActor"));
         
-        testActor.tell("ready spring boot",null);        
-
+        //액터 생성... 
+        ActorRef testActor = system.actorOf(ext.props("testActor"),"service1");
+        
+        ActorSelection testActor2 = system.actorSelection("user/service1");
+        
+        ActorSelection testActorRemote = system.actorSelection("akka.tcp://app@localhost:2552/user/service1");
+        
+        //참조를 통한 전송 ( 로컬 객체관리에 유리 )
+        testActor.tell("ready spring boot",null);
+        
+        //액터 셀렉션을 통한 전송(
+        testActor2.tell("ready spring boot -again", null);
+        
+        //리모트를 통한 전송
+        testActorRemote.tell("ready spring boot -again too", null);
+        
+        
 	}
 }
